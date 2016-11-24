2d5d838e5496943e54a25129a90b8f9c5a4c2b4b
Crab man <cartoonist21@gmail.com>
2016-11-23 17:27:02 -0800

    Add a line of code.

---
+++ TeamProject1/TeamProject1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//initial check in.
+			Console.WriteLine("Hello, World!");
+			//Add a line of code.
+
+
+
+			Console.ReadKey();
+		}
+	}
+}
